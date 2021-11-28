e00bcc1f3e410ea9a3bf87fb6af0149b825f4211
Deszip <raf.rafiki@gmail.com>
2021-11-28 21:01:24 +0200

    themis dep

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         .binaryTarget(
             name: "AppSpectorSDKE2E",
             url: "https://github.com/appspector/ios-sdk/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "78064690dcd0a7127ac8e013ec29ca0b4ec61620b31921d181b505059c3520b8"
+            checksum: "78064690dcd0a7127ac8e013ec29ca0b4ec61620b31921d181b505059c3520b8",
+            dependencies: ["themis"]
         )
     ]
 )
