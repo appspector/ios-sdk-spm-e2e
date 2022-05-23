37fa41f0046f4b063248377cecbfad7c24a89bb4
Deszip <raf.rafiki@gmail.com>
2022-05-23 21:50:54 +0300

    checksum updated

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "AppSpectorSDKE2E",
             url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "a98725040f7598bee89e267510f3d7bc9b9b5b373a53e688c70a007ca2f44f23"
+            checksum: "bb978a02890958ca7d40c1b36421bf400ebfb30c9d737becf629c6a7ec83e163"
         ),
         .binaryTarget(
             name: "themis",
