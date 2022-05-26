c0efe5566e31a9a52296d293b180d37c207f64aa
Deszip <raf.rafiki@gmail.com>
2022-05-26 23:15:16 +0300

    subpath

--- Package.swift
+++ Package.swift
@@ -77,13 +77,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AppSpectorSDKE2E",
-            path: "AppSpectorSDKE2E.xcframework"
+            path: "targets/AppSpectorSDKE2E.xcframework"
 //            url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
 //            checksum: "bb978a02890958ca7d40c1b36421bf400ebfb30c9d737becf629c6a7ec83e163"
         ),
         .binaryTarget(
             name: "themis",
-            path: "themis.xcframework"
+            path: "targets/themis.xcframework"
         )
 //        .binaryTarget(name: "themis",
 //                      url: "https://github.com/cossacklabs/themis/releases/download/0.14.0/themis.xcframework.zip",
