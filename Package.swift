2aa7ac759c252a77f6a77dc992ded539b59af749
Deszip <raf.rafiki@gmail.com>
2022-05-26 22:49:47 +0300

    local path for sdk

--- Package.swift
+++ Package.swift
@@ -77,8 +77,9 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AppSpectorSDKE2E",
-            url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "bb978a02890958ca7d40c1b36421bf400ebfb30c9d737becf629c6a7ec83e163"
+            path: "AppSpectorSDKE2E.xcframework.zip"
+//            url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
+//            checksum: "bb978a02890958ca7d40c1b36421bf400ebfb30c9d737becf629c6a7ec83e163"
         ),
         .binaryTarget(name: "themis",
                       url: "https://github.com/cossacklabs/themis/releases/download/0.14.0/themis.xcframework.zip",
