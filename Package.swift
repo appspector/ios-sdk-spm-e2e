ef85b8266de7e1df0f239696e7d963d72ae07bec
Deszip <deszip@DuckBox.local>
2022-10-17 21:51:42 +0300

    Themis update

--- Package.swift
+++ Package.swift
@@ -1,25 +1,6 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let package = Package(
-    name: "AppSpector",
-    platforms: [
-        .iOS(.v13)
-    ],
-    products: [
-        .library(
-            name: "AppSpectorSDKE2E",
-            targets: ["AppSpectorSDKE2E"])
-    ],
-    targets: [
-        .binaryTarget(
-            name: "AppSpectorSDKE2E",
-            url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "bb978a02890958ca7d40c1b36421bf400ebfb30c9d737becf629c6a7ec83e163"
-        )
-    ]
-)
-
 //let package = Package(
 //    name: "AppSpector",
 //    platforms: [
@@ -28,36 +9,55 @@ let package = Package(
 //    products: [
 //        .library(
 //            name: "AppSpectorSDKE2E",
-//            targets: ["AppSpectorUmbrella"])
-//    ],
-//    dependencies: [
-//        .package(
-//            name: "themis",
-//            url: "https://github.com/cossacklabs/themis/releases/download/0.13.12/themis.xcframework.zip",
-//            from: "0.13.12"
-//        )
+//            targets: ["AppSpectorSDKE2E"])
 //    ],
 //    targets: [
 //        .binaryTarget(
 //            name: "AppSpectorSDKE2E",
 //            url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
 //            checksum: "bb978a02890958ca7d40c1b36421bf400ebfb30c9d737becf629c6a7ec83e163"
-//        ),
-//        .target(
-//            name: "AppSpectorUmbrella",
-//            dependencies: [
-//                .target(name: "AppSpectorSDKE2E"),
-//                .target(name: "themis")
-//            ],
-//            path: "./"
-//        ),
-//        .binaryTarget(
-//            name: "themis",
-//            url: "https://github.com/cossacklabs/themis/releases/download/0.13.12/themis.xcframework.zip",
-//            checksum: "c74f65d4918884220efe99c3195001fa8aabc8030ad85f8ef30d2bfed11065a3"
 //        )
 //    ]
 //)
+
+let package = Package(
+    name: "AppSpector",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "AppSpectorSDKE2E",
+            targets: ["AppSpectorUmbrella"])
+    ],
+    dependencies: [
+        .package(
+            name: "themis",
+            url: "https://github.com/cossacklabs/themis/releases/download/0.14.0/themis.xcframework.zip",
+            from: "0.14.0"
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppSpectorSDKE2E",
+            url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
+            checksum: "bb978a02890958ca7d40c1b36421bf400ebfb30c9d737becf629c6a7ec83e163"
+        ),
+        .target(
+            name: "AppSpectorUmbrella",
+            dependencies: [
+                .target(name: "AppSpectorSDKE2E"),
+                .target(name: "themis")
+            ],
+            path: "./"
+        ),
+        .binaryTarget(
+            name: "themis",
+            url: "https://github.com/cossacklabs/themis/releases/download/0.14.0/themis.xcframework.zip",
+            checksum: "182587c9070b94e552816d3cc174d64ea0f0a64d5b19483210bcf3dcac4d2658"
+        )
+    ]
+)
 
 //let package = Package(
 //    name: "AppSpector",
