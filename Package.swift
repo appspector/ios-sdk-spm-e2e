28bbac060bf2a8e6f2c38c20143acbd4c988e364
Deszip <deszip@DuckBox.local>
2022-10-17 22:12:13 +0300

    SDK updated to 1.4.7

--- Package.swift
+++ Package.swift
@@ -40,8 +40,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppSpectorSDKE2E",
-            url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "bb978a02890958ca7d40c1b36421bf400ebfb30c9d737becf629c6a7ec83e163"
+            url: "https://github.com/appspector/ios-sdk-spm-e2e/releases/download/1.4.7/AppSpectorSDKE2E.xcframework.zip",
+            checksum: "41c2a0f933b6c7fccda3f84431f218bebc00de4958380396c1eca57e58ef822f"
         )
 //        .target(
 //            name: "AppSpectorUmbrella",
